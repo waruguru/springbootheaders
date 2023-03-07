4b2102be8f688af9592f4ba10ece53b099d923ca
Joyce Waruguru Muriithi <JMURIITHI4@safaricom.co.ke>
2023-03-07 10:35:03 +0300

    First commit

---
+++ src/main/java/com/waruguru/msheadersservice/TestHeadersApplication.java
@@ -0,0 +1,13 @@
+package com.waruguru.msheadersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestHeadersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestHeadersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/waruguru/msheadersservice/TestHeadersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waruguru.msheadersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestHeadersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
