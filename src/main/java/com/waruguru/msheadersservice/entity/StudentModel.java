486381d8b9fd002d32891507b4681e06fd8c16ce
Joyce Waruguru Muriithi <JMURIITHI4@safaricom.co.ke>
2023-03-07 10:49:25 +0300

    Add entity class

---
+++ src/main/java/com/waruguru/msheadersservice/entity/StudentModel.java
@@ -0,0 +1,34 @@
+package com.waruguru.msheadersservice.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="student")
+public class StudentModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String fullName;
+    private String courseName;
+    private long age;
+    private String createdBy;
+    @JsonIgnore
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @JsonIgnore
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
